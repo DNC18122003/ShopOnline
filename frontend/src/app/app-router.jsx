2ae59fca3a29d17cf11fd89921a35b4e84c05258
DNC18122003 <dinhcuonglc2003@gmail.com>
2026-01-06 11:09:07 +0700

    no message

--- frontend/src/app/app-router.jsx
+++ frontend/src/app/app-router.jsx
@@ -1,1 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
+// import {
+
+// } from "@/components/layouts";
+import { NotFound } from '@/pages';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hẹ Hẹ Hẹ</div>,
+        children: [],
+    },
+
+    {
+        path: '*',
+        element: <NotFound />,
+    },
+]);
