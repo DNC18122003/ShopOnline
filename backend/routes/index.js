3b7f22e03630712c5d7420d07cd88a67ca157e80
KetXanh <169112526+KetXanh@users.noreply.github.com>
2026-01-23 16:34:24 +0700

    Order controller & router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const uploadRouter = require("./upload");
 const authRouter = require("./auth-router");
-const cartRouter = require("./order/cart.route")
+const cartRouter = require("./order/cart.route");
+const orderRouter = require("./order/order.route");
 module.exports = (app) => {
   const api = "/api";
   app.use(api + "/upload", uploadRouter);//Test upload
   app.use(api + "/auth", authRouter);
   app.use(api + "/cart", cartRouter);
+  app.use(api + "order", orderRouter);
 };
 
 
