216c79021ab6b27cf5bc7b6d3880298be9af6bd1
NTTrungCode <trungnthe171142@fpt.edu.vn>
2026-01-28 09:29:17 +0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,16 @@ const uploadRouter = require("./upload");
 const authRouter = require("./auth-router");
 const cartRouter = require("./order/cart.route");
 const orderRouter = require("./order/order.route");
+const blogRouter = require("./Blog/blog-router");
+const discountRouter = require('./discount/discount-router');
 module.exports = (app) => {
   const api = "/api";
   app.use(api + "/upload", uploadRouter);//Test upload
   app.use(api + "/auth", authRouter);
   app.use(api + "/cart", cartRouter);
   app.use(api + "order", orderRouter);
+  app.use(api + "/blog", blogRouter);
+  app.use(api + "/discount", discountRouter);
 };
 
 
