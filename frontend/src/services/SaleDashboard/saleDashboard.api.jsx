791c46fed70b100d9fb74ffe9b43688c096f4e92
NTTrungCode <trungnthe171142@fpt.edu.vn>
2026-03-28 23:24:19 +0700

    FixDashboard

--- frontend/src/services/SaleDashboard/saleDashboard.api.jsx
+++ frontend/src/services/SaleDashboard/saleDashboard.api.jsx
@@ -2,11 +2,12 @@ import customizeAPI from '../customizeApi';
 
 const saleDashboardService = {
     // Lấy dữ liệu tổng quan cho dashboard theo tháng và năm
-    getDashboardSummary: (month, year) => {
+    getDashboardSummary: (month, year, userId) => {
         return customizeAPI.get('/dashboard/summary', {
             params: {
                 month: month,
                 year: year,
+                userId: userId,
             },
         });
     },
